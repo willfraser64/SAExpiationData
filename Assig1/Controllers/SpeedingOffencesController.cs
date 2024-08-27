6f0bb0aef04944955b54cb3f1eacf0bd2af1513a
willfraser64 <wcfpvp@gmail.com>
2024-08-27 22:38:44 +0930

    Created SpeedingOffences controller.

---
+++ Assig1/Controllers/SpeedingOffencesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Assig1.Controllers
+{
+    public class SpeedingOffencesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
